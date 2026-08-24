dd90059e1ee1ccc8557517d7e1cc958bf217d1a9
madebycli <root.cli@proton.me>
2026-08-24 20:47:41 +0200

    Mount opt-in map diagnostics

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AccessRecoveryGate } from "./access/AccessRecoveryGate";
+import { MapDiagnostics } from "./diagnostics/MapDiagnostics";
 import "./styles.css";
 import "./street-mode.css";
 import "./mobile-stability.css";
 import "./svg-overlay.css";
 import "./m4.css";
 import "./access-recovery.css";
+import "./diagnostics/map-diagnostics.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <AccessRecoveryGate />
+    <MapDiagnostics />
   </StrictMode>,
 );
