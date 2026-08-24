ef826be337bc32fae670cfe42357789841b8a4da
madebycli <root.cli@proton.me>
2026-08-24 15:34:45 +0200

    Mount admin recovery gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { AccessRecoveryGate } from "./access/AccessRecoveryGate";
 import "./styles.css";
 import "./street-mode.css";
 import "./mobile-stability.css";
 import "./svg-overlay.css";
 import "./m4.css";
+import "./access-recovery.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <AccessRecoveryGate />
   </StrictMode>,
 );
